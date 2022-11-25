f0718a096751ff31bbb84ad103580d84a50f559d
joris neblai <joris.neblai@gmail.com>
2022-11-25 17:37:03 +0100

    adding a drawer menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import './reset.css';
 import Form from './Components/Form/Form';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <div className="header-top-block">
-          <h1 className="header-top-block_title">
-            solinum
-          </h1>
-      </div>
+      <NavBar />
       <Form />
     </div>
   );
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,67 @@
+import { AppBar, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import CreateIcon from '@mui/icons-material/Create';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import MapIcon from '@mui/icons-material/Map';
+import { Box } from '@mui/system';
+import React from 'react';
+
+const drawerWidth = 240;
+
+const NavBar = () => {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+            >
+                <Toolbar>
+                    <Typography variant="h6" noWrap component="div">
+                        Solinum
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar />
+                <Divider />
+                <List>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <CreateIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Formulaire de contribution" />
+                    </ListItemButton>
+                    <Divider />
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <FormatListBulletedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Point d'intérêt géolocalisés" />
+                    </ListItemButton>
+                    <Divider />
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <MapIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Carte" />
+                    </ListItemButton>
+
+                </List>
+
+            </Drawer>
+        </Box>
+    )
+}
+
+export default NavBar;
