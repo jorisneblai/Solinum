59cfd3003a0fa6839306457cf50c935ce0fbef27
joris neblai <joris.neblai@gmail.com>
2022-11-30 14:31:02 +0100

    fixing dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import './reset.css';
-import { Routes, Route } from 'react-router-dom';
-import Form from './Components/Form/Form';
-import NavBar from './Components/NavBar/NavBar';
-import PoiList from './Components/PoiList/PoiList';
+// import { Routes, Route } from 'react-router-dom';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   return (
     <main className="App">
-      <Routes>
-        <Route path="/" element={<><NavBar/> <Form /></>} />
-        <Route path="/poilist" element={<><NavBar/> <PoiList /></>} />
-        <Route path="/map" element={<><NavBar/></>} />
-      </Routes>
+      <Dashboard />
     </main>
   );
 }
